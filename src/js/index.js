8c6bd8635232bd657a0488314743731a3e093f1e
Ken A Collins <Ken@KenACollins.com>
2019-07-02 10:58:41 -0400

    Finalized webpack configuration by adding support for CSS files and the MLB background image.

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,6 @@
 // index.js - main entry point into application
+import '../css/custom.css';
+import BackgroundImage from '../images/mlb_background.jpg';
 import Controller from './Controller.js';
 
 new Controller();
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,17 @@ module.exports = {
     output: {                                   // Where output of webpack processing will go.
         filename: 'bundle.js',                  // Any name you want to specify for the bundled code, eg., main.js or bundle.js.
         path: path.resolve(__dirname, 'dist')   // Directory name off root of project where bundle.js will be created. FYI, 'dist' is short for 'distribution'.
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,                 // Regular expression tells webpack to identify CSS files by .css extension.
+                use: [ 'style-loader', 'css-loader' ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,   // Regular expression tells webpack to identify image files by various extensions.
+                use: [ 'file-loader' ]
+            }
+        ]
     }
 };
